2121b2a152de33144f61a62fd3ef1e4938996fe6
4gcnny5cxp-dot <4gcnny5cxp@privaterelay.appleid.com>
2026-01-13 13:08:19 +0100

    Delete pages/vitache directory

--- pages/vitache/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function VitaCheck() {
-  return (
-    <main style={{ padding: "40px", maxWidth: "700px", margin: "0 auto" }}>
-      <h1>VitaCheck</h1>
-      <p>Demo – formulier volgt.</p>
-      <p><a href="/">← Terug</a></p>
-    </main>
-  );
-}
